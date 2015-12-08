785c4c103fe438c731cfb5845c62b14273b8c662
Quang <quangdx.fuky@gmail.com>
2015-12-08 17:03:06 +0700

    - Add scrolling background

--- Technical/LuckyLuke/Assets/Scripts/Coin.cs
+++ Technical/LuckyLuke/Assets/Scripts/Coin.cs
@@ -4,19 +4,25 @@ public class Coin : MonoBehaviour
 {
     public float moveSpeed;
     public Vector3 targetPos;
+    public Vector2 moveVector2;
 
 	// Use this for initialization
 	void Start ()
 	{
-	   
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    Move();
+	   moveVector2 = new Vector2(1*moveSpeed, 0*moveSpeed);
 	}
 
-    void Move()
+    void Update()
+    {
+        Despawn();
+    }
+
+    //void FixedUpdate()
+    //{
+    //    rigidbody2D.velocity = moveVector2;
+    //}
+
+    void Despawn()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed);
         if (transform.position == targetPos)
---
+++ Technical/LuckyLuke/Assets/ScrollBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollBackground : MonoBehaviour {
+
+    public float scrollSpeed = 0.5f;
+    public Renderer rend;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float offset = (Time.time)  * scrollSpeed;
+	    float offset2 = offset%1.0f;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset2, 0));
+	}
+}
